2c89cebce3fa22a2f6dd187db005635169207d67
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-09 18:57:28 +0800

    Scaffold read_csaf_demo structure

---
+++ csaf_database/examples/read_csaf_demo.rs
@@ -0,0 +1,11 @@
+//! CSAF 数据库读取示例程序
+//!
+//! 该示例程序演示了如何使用 csaf_database crate 从数据库读取 CSAF 相关数据。
+
+use csaf_database::{CsafQuery, DatabaseConfig, DatabaseManager};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    todo!()
+}
