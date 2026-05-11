df446b56cb101cda810da33598cfe1eb9d87e3b7
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-11 18:28:16 +0800

    Scaffold examples modules (csaf_to_database)

---
+++ database/examples/csaf_to_database.rs
@@ -0,0 +1,12 @@
+//! 解析CSAF文件，转换为OVAL格式并存储到数据库的示例程序
+
+use csaf::CSAF;
+use database::{DatabaseConfig, DatabaseManager};
+use parser::{csaf_to_oval, process_csaf_id};
+use std::env;
+use utils::config::AppConfig;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
