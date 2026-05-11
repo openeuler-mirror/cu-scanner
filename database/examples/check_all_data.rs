c60dcd7f99b2fc42369098d98efa8f1b7057970d
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-11 13:30:17 +0800

    Scaffold examples modules structure

---
+++ database/examples/check_all_data.rs
@@ -0,0 +1,9 @@
+//! 检查数据库中所有数据的示例程序
+
+use database::{DatabaseConfig, DatabaseManager};
+use utils::config::AppConfig;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
