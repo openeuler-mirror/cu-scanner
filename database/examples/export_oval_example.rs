4b52d398ef6cfe088283f522af735f17a635b54e
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-12 09:29:38 +0800

    Lay out skeleton examples with docstrings

---
+++ database/examples/export_oval_example.rs
@@ -0,0 +1,10 @@
+//! 从数据库导出OVAL文件的示例程序
+
+use database::{DatabaseConfig, DatabaseManager};
+use std::fs;
+use utils::config::AppConfig;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
