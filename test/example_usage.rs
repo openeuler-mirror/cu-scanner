990833d83346d471550bfe2178baf6c7ec773645
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-12 16:27:01 +0800

    Build example_usage main

--- test/example_usage.rs
+++ test/example_usage.rs
@@ -3,7 +3,14 @@
 use test_utils::TestConfig;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    todo!()
+    // 从配置文件加载测试配置
+    let config = TestConfig::load_from_file("test_config.toml")?;
+
+    println!("=== CSAF测试文件 ===");
+    for file in config.get_csaf_files() {
+        println!("  {}", file);
+    }
+    todo!();
 }
 
 #[cfg(test)]
