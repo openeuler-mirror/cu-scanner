316c9ab7e05b36b9f71996c806790f0be0730fd7
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-14 02:37:55 +0800

    Hook up core create_definition_from_sa_id functionality

--- parser/examples/create_definition_from_sa_id.rs
+++ parser/examples/create_definition_from_sa_id.rs
@@ -8,5 +8,18 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    todo!()
+    println!("=== 根据 SA ID 创建 OVAL 定义示例 ===\n");
+
+    // 配置 CSAF 数据库连接
+    let csaf_db_config = DatabaseConfig::new(
+        "82.156.53.132", // 数据库主机
+        5432,            // 数据库端口
+        "cu_cveadmin",   // 数据库名称
+        "cu_cveadmin",   // 用户名
+        "Ninjia1000SX",  // 密码
+    );
+
+    println!("CSAF 数据库配置:");
+    println!("  主机: {}", csaf_db_config.host);
+    todo!();
 }
