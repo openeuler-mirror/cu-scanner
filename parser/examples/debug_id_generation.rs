352f202197019cac64afdc09c2bfdc71011cd9fb
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-14 03:08:02 +0800

    Create debug_id_generation main

--- parser/examples/debug_id_generation.rs
+++ parser/examples/debug_id_generation.rs
@@ -4,5 +4,12 @@ use csaf::CSAF;
 use parser::csaf_to_oval_with_counter;
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    todo!()
+    println!("调试ID生成示例程序");
+
+    // 加载第一个CSAF文件
+    println!("加载第一个CSAF文件...");
+    let csaf1 = CSAF::from_file(
+        "/home/fatmouse/workspace/cu-scanner/test/csaf/csaf-openeuler-sa-2025-1004.json",
+    )?;
+    todo!();
 }
