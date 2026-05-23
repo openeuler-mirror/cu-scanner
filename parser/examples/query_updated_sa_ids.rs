52f658e9732347a949ad7b62277b35cb94119322
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-23 18:59:01 +0800

    Add integration test for query_updated_sa_ids

--- parser/examples/query_updated_sa_ids.rs
+++ parser/examples/query_updated_sa_ids.rs
@@ -33,5 +33,29 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // - "2025-01-01 00:00:00"
     // - "2025-10-01T00:00:00"
     let timestamp = "2025-10-01";
-    todo!();
+    println!("查询更新时间在 {} 之后的所有 SA ID...\n", timestamp);
+
+    // 查询更新时间之后的 SA ID
+    match get_sa_ids_after_updated_time(&csaf_db_config, timestamp).await {
+        Ok(sa_ids) => {
+            println!("成功查询到 {} 个 SA ID:\n", sa_ids.len());
+
+            // 显示所有 SA ID
+            if sa_ids.is_empty() {
+                println!("  (没有找到符合条件的 SA ID)");
+            } else {
+                for (i, sa_id) in sa_ids.iter().enumerate() {
+                    println!("  {}. {}", i + 1, sa_id);
+                }
+            }
+
+            println!("\n查询成功！");
+        }
+        Err(e) => {
+            eprintln!("查询 SA ID 失败: {}", e);
+            return Err(e);
+        }
+    }
+
+    Ok(())
 }
