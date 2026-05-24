98bf64f117bde2de27c6fe0ef15c8c42a784245b
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-24 22:06:25 +0800

    Implement test_csaf_to_oval_with_os_info main

--- database/examples/test_csaf_to_oval_with_os_info.rs
+++ database/examples/test_csaf_to_oval_with_os_info.rs
@@ -3,5 +3,9 @@ use csaf::CSAF;
 use parser::csaf_to_oval;
 use std::error::Error;
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    todo!()
+    println!("测试CSAF到OVAL转换（包含OS信息匹配）\n");
+    // 加载CSAF测试文件
+    println!("加载CSAF测试文件...");
+    let csaf = CSAF::from_file("../test/csaf/csaf-openeuler-sa-2025-1004.json")?;
+    todo!();
 }
