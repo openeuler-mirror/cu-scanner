759c0cf6aff72a90f50afea925b91362acd15164
qiucx15 <qiucx15@chinaunicom.cn>
2026-05-25 18:07:44 +0800

    Implement test_os_info_id_auto_fill main (test_os_info_id_auto_fill 2)

--- database/examples/test_os_info_id_auto_fill.rs
+++ database/examples/test_os_info_id_auto_fill.rs
@@ -11,5 +11,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 从配置文件加载数据库配置
     let config = AppConfig::from_file("config/cu-scanner.toml")?;
+    let db_config = DatabaseConfig::new(
+        &config.database.host,
+        config.database.port,
+        &config.database.database,
+        &config.database.username,
+        &config.database.password,
+    );
+
+    // 连接数据库
+    let mut db_manager = DatabaseManager::new(&db_config).await?;
     todo!();
 }
