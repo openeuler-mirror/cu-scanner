fb5b9adb91d7344a3b73ac8dff9ed03fb7fd803a
Chen Linxuan <chenlx163@chinaunicom.cn>
2026-08-07 11:37:41 +0800

    Simplify Option::map(...).unwrap_or(0) to map_or in oval
    Four places used .as_ref().map(|x| x.len()).unwrap_or(0). Use the
    more idiomatic .map_or(0, |x| x.len()) which avoids constructing an
    intermediate Option<usize>.

--- oval/src/lib.rs
+++ oval/src/lib.rs
@@ -353,7 +353,7 @@ impl OvalDefinitions {
               self.definitions.items.len(),
               self.tests.rpminfo_tests.len(),
               self.objects.rpm_info_objects.len(),
-              self.states.rpminfo_states.as_ref().map(|s| s.len()).unwrap_or(0));
+              self.states.rpminfo_states.as_ref().map_or(0, |s| s.len()));
 
         Ok(())
     }
@@ -619,7 +619,7 @@ impl Metadata {
 
     /// 获取引用数量
     pub fn get_reference_count(&self) -> usize {
-        self.references.as_ref().map(|r| r.len()).unwrap_or(0)
+        self.references.as_ref().map_or(0, |r| r.len())
     }
 }
 
@@ -1047,7 +1047,7 @@ impl Criteria {
 
     /// 获取子条件数量
     pub fn get_sub_criteria_count(&self) -> usize {
-        self.sub_criteria.as_ref().map(|s| s.len()).unwrap_or(0)
+        self.sub_criteria.as_ref().map_or(0, |s| s.len())
     }
 
     /// 设置操作符
@@ -1721,7 +1721,7 @@ impl States {
 
     /// 获取状态数量
     pub fn len(&self) -> usize {
-        self.rpminfo_states.as_ref().map(|s| s.len()).unwrap_or(0)
+        self.rpminfo_states.as_ref().map_or(0, |s| s.len())
     }
 
     /// 检查是否为空
